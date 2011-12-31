8d3f16f69f55c893675ae7444fffaa8abe08ff69
satkumartomer <satkumartomer@gmail.com>
2011-12-31 11:52:25 +0000

    Final changes for the 0.2.0 release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 import sys, os
 
-version = '0.1.1'
+version = '0.2.0'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
@@ -19,7 +19,7 @@ setup(
     author = "Sat Kumar Tomer",
     author_email = "satkumartomer@gmail.com",
     url = "http://ambhas.com/",
-    download_url = "http://ambhas.com/tools/ambhas-0.0.5.tar.gz",
+    download_url = "http://pypi.python.org/pypi/ambhas/",
     keywords = ["ambhas", "hydrology", "statistics", "modelling"],
     packages = find_packages(exclude=['ez_setup', 'examples', 'tests']),
     include_package_data=True,
