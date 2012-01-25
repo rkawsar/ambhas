95ff4efc2ff6b7bb4ce1d758bba7e6e382ee5a3c
satkumartomer@gmail.com <satkumartomer@gmail.com@6c2eb14a-26ea-015c-4a4b-5c625288b75b>
2012-01-25 10:45:15 +0000

    Creating the 0.3.1 tag

---
+++ tags/0.3.1/setup.py
@@ -0,0 +1,50 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+import sys, os
+
+version = '0.1.1'
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name = "ambhas",
+    version = version,
+    description = "A library devloped under the project AMBHAS",
+    author = "Sat Kumar Tomer",
+    author_email = "satkumartomer@gmail.com",
+    url = "http://ambhas.com/",
+    download_url = "http://pypi.python.org/pypi/ambhas/",
+    keywords = ["ambhas", "hydrology", "statistics", "modelling"],
+    packages = find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe = False,
+    install_requires=["numpy"],
+    classifiers = [
+        "Programming Language :: Python :: 2.7",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        ],
+    long_description = (
+    "\n"+read('docs/index.txt')
+    + '\n' + '\n'
+    + read('CHANGELOG.txt')
+    + '\n' + '\n'
+    +'License\n'
+    +'========\n'
+    + 'Please read LICENSE.text available with the library.'
+    + '\n'
+    + 'Download\n'
+    + '========\n'
+    ),
+)
+
