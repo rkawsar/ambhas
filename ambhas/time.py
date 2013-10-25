e6e78cce925f5a7fb853f85f27a87f5670f9a0a3
satkumartomer <satkumartomer@gmail.com>
2013-10-25 09:22:47 +0000

    doy2md function added to time

--- ambhas/time.py
+++ ambhas/time.py
@@ -18,3 +18,16 @@ def ymd2doy(year, month, day):
     for i in xrange(n):
         doy[i] = dt.datetime(year[i], month[i], day[i]).toordinal()-dt.datetime(year[i], 1, 1).toordinal()+1
     return doy
+
+def doy2md(doy,year):
+    foo = dt.date.fromordinal(dt.date(year,1,1).toordinal()+doy-1)
+    month = foo.month
+    day = foo.day
+    return month,day
+    
+    
+
+if __name__=='__main__':
+    year = 2011
+    doy = 365
+    print doy2md(doy,year)
